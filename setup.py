c4807306ac65d42f2c414169575343cbabf64ff8
Zigfrid Zvezdin <ziggerzz@gmail.com>
2020-03-30 16:58:45 +0200

    Add more python versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="in_container",
-    version="0.3",
+    version="0.4",
     author="Zigfrid Zvezdin",
     author_email="ziggerzz@gmail.com",
     description="Check if running inside a Docker container",
@@ -18,4 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
