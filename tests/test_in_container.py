9df661c6198b83d5a806835d6be269471669e4fd
Zigfrid Zvezdin <ziggerzz@gmail.com>
2020-04-07 23:14:46 +0200

    Apparently Travis builds not in a container

--- tests/test_in_container.py
+++ tests/test_in_container.py
@@ -1,5 +1,5 @@
 import in_container
 
 
-def test_in_container_travis_build():
-    assert in_container.in_container() is True
+def test_in_container_on_host():
+    assert in_container.in_container() is False
